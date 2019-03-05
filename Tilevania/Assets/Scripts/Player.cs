95d4f858d415f37b9da5f7107195d300353fef38
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-03-04 23:23:47 -0700

    Added tile collision and basic running to Player

---
+++ Tilevania/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D myRigidBody;
+    Animator animator;
+    [SerializeField] float runSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       myRigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        if (controlThrow != 0)
+        {
+            animator.SetBool("Running", true);
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+        }
+        myRigidBody.velocity = playerVelocity;
+    }
+}
