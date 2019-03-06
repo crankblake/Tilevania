b297727b7ff85ff01e2b1257f5094ca366c0563b
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-03-06 00:35:06 -0700

    Jumping

--- Tilevania/Assets/Scripts/Player.cs
+++ Tilevania/Assets/Scripts/Player.cs
@@ -6,35 +6,57 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody2D myRigidBody;
-    Animator animator;
+    //Config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 10f;
+    //State
+    bool isAlive = true;
 
-    // Start is called before the first frame update
+    //Cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    
+
+    //Message then methods
     void Start()
     {
        myRigidBody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+       myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        FlipSprite();
+        Jump();
     }
 
     private void Run()
     {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
-        if (controlThrow != 0)
-        {
-            animator.SetBool("Running", true);
-        }
-        else
-        {
-            animator.SetBool("Running", false);
-        }
         myRigidBody.velocity = playerVelocity;
+        Debug.Log("Player velocity is: " + playerVelocity);
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x),1f);
+        }
+    }
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
     }
 }
