149c4e63fcfdb38791f3387c6aa041fa419a233b
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-03-20 23:59:47 -0600

    Level Exit

---
+++ Tilevania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 3f;
+    [SerializeField] float LevelExitSlowMoFactor = .2f;
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+         StartCoroutine("LoadNextLevel");
+    }
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = LevelExitSlowMoFactor;
+        yield return new WaitForSeconds(levelLoadDelay);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+
+}
