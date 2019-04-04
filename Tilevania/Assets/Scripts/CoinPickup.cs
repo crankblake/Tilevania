afccb98e8f7a34024baa151cdcbe29c14fb4ac8e
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-04-03 23:02:55 -0600

    Coins!

---
+++ Tilevania/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("Collider2D is " + collision);
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
