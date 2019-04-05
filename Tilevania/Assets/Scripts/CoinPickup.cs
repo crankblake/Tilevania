97880e1cd1a67eb0625820bcddd37fe285bbfeee
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-04-04 23:07:18 -0600

    sound on Coin pickup

--- Tilevania/Assets/Scripts/CoinPickup.cs
+++ Tilevania/Assets/Scripts/CoinPickup.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickupSFX;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Debug.Log("Collider2D is " + collision);
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
     }
